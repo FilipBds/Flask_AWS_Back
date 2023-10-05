25b76b47549a29477cbdfca93608addbb1afcbef
FilipBds <116580112+FilipBds@users.noreply.github.com>
2023-10-05 17:35:05 +0300

    Update application.py

--- application.py
+++ application.py
@@ -21,6 +21,7 @@ bcrypt = Bcrypt(application)
 jwt = JWTManager(application)
 
 # Connect to MongoDB
+# Connect to MongoDB
 try:
     client = pymongo.MongoClient(
         "mongodb+srv://Mark_Jeff:1234@cluster0.0jh9bwk.mongodb.net/?retryWrites=true&w=majority")
@@ -28,12 +29,89 @@ try:
     print("Connected")
     collection = db.users
     collection1 = db.posts
+    collection2 = db.notifications
+    collection3 = db.private_box
 
     collections = collection.find()
     print("Connected")
 
 except pymongo.errors.ConnectionFailure as e:
     print("Could not connect to MongoDB: %s" % e)
+
+
+@application.route('/add_notification', methods=['POST'])
+@jwt_required()
+def add_notification():
+
+    post_id = request.json.get('post_id')
+    username = get_jwt_identity()
+    msg1 = username + request.json.get('message')
+    message = {'message': msg1, 'username': username, 'post_id': post_id}
+
+    post = collection1.find_one({'_id': ObjectId(post_id)})
+    reciver = post['username']
+    type = request.json.get('type')
+    if type == '':
+        notifications = collection2.find_one({'username': reciver})
+        if notifications:
+            notifications['messages'].append(message)
+            collection2.update_one({'username': reciver}, {
+                                   '$set': notifications})
+            return jsonify({'message': 'ok'})
+        else:
+            new_notification_object = {
+                'username': reciver, 'messages': [message]}
+            collection2.insert_one(new_notification_object)
+            return jsonify({'message': 'ok'})
+
+    else:
+        return jsonify({'message': 'ok'})
+
+
+@application.route('/get_notifications', methods=['POST'])
+@jwt_required()
+def get_notification():
+
+    username = get_jwt_identity()
+
+    type = request.json.get('type')
+    if type == '':
+        notifications = collection2.find_one({'username': username})
+        if notifications:
+
+            return jsonify({'messages': notifications['messages']})
+        else:
+            new_notification_object = {'username': username, 'messages': []}
+            collection2.insert_one(new_notification_object)
+            return jsonify({'message': []})
+
+    else:
+        return jsonify({'message': 'ok'})
+
+
+@application.route('/delete_notifications', methods=['POST'])
+@jwt_required()
+def delete_notification():
+    nots = request.json.get('new_list')
+
+    username = get_jwt_identity()
+
+    type = request.json.get('type')
+    if type == '':
+        notifications = collection2.find_one({'username': username})
+
+        if notifications:
+            notifications['messages'] = nots
+            collection2.update_one({'username': username}, {
+                                   '$set': notifications})
+
+            return jsonify({'messages': 'deleted'})
+        else:
+
+            return jsonify({'message': 'ok'})
+
+    else:
+        return jsonify({'message': 'ok'})
 
 
 @application.route('/sign_up', methods=['POST'])
@@ -61,7 +139,7 @@ def sign_up():
         'password': hashed_password,
         'username': username,
         'name': name,
-        'profile_picture': 'http://www.goodmorningimagesdownload.com/wp-content/uploads/2021/12/Best-Quality-Profile-Images-Pic-Download-2023.jpg',
+        'profile_picture': random.choice(urls),
         'subscriptions': [],
         'boxes': []
     }
@@ -125,11 +203,12 @@ def add_post():
         print(current_user)
         print(permissions)
 
-        if current_user in permissions:
+        if current_user in permissions or current_user == post['username']:
             dict_message = {"username": current_user, "content": message}
 
             # Add the message to the post's messages list
             post['messages'].append(dict_message)
+            post['post'] += 1
 
             # Update the post in the collection
             collection1.update_one({'_id': ObjectId(post_id)}, {'$set': post})
@@ -138,6 +217,38 @@ def add_post():
             return jsonify({'message': 'User does not have permission to write on this post'}), 200
 
     return jsonify({'message': 'Post not found'}), 200
+
+
+@application.route('/add_message_w', methods=['POST'])
+@jwt_required()
+def add_post_w():
+    current_user = get_jwt_identity()
+    data = request.get_json()
+    post_id = data.get('post_id')
+    message = data.get('message')
+
+    # Find the post with the given ID
+    post = collection3.find_one({'_id': ObjectId(post_id)})
+
+    if post:
+        # Check if the username has permission to write on the post
+
+        print("post")
+        print(current_user)
+
+        dict_message = {"username": current_user, "content": message}
+
+        # Add the message to the post's messages list
+        post['messages'].append(dict_message)
+        post['post'] += 1
+
+        # Update the post in the collection
+        collection1.update_one({'_id': ObjectId(post_id)}, {'$set': post})
+        return jsonify({'message': 'Post updated successfully'}), 200
+
+    return jsonify({'message': 'Post not found'}), 200
+
+# w
 
 
 @application.route('/add_to_myboxes', methods=['POST'])
@@ -169,6 +280,28 @@ def add_to_myboxes():
 @jwt_required()
 def get_myboxes():
     current_user = get_jwt_identity()
+    print(current_user)
+
+    # Find the user with the given username
+    user = collection.find_one({'username': current_user})
+
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
+    # Retrieve post IDs from the collection based on the current user
+    post_ids_cursor = collection1.find(
+        {'username': current_user}, {'_id': 1, })
+
+    # Create a list of dictionaries containing _id and postId
+    post_ids_list = [str(post['_id']) for post in post_ids_cursor]
+
+    return jsonify({'myBoxes': post_ids_list})
+
+
+@application.route('/get_user_box', methods=['POST'])
+def get_user_boxes():
+    data = request.get_json()
+    current_user = data.get('username')
     print(current_user)
 
     # Find the user with the given username
@@ -219,6 +352,7 @@ def subscribe():
     data = request.get_json()
     # The single post ID the user wants to subscribe to
     post_id = data.get('post_id')
+    post = collection1.find_one({'_id': ObjectId(post_id)})
 
     # Find the user with the given username or email
     user = collection.find_one(
@@ -231,6 +365,8 @@ def subscribe():
     current_subscriptions = user.get('subscriptions', [])
     if post_id not in current_subscriptions:
         current_subscriptions.append(post_id)
+        post['subs'] += 1
+        collection1.update_one({'_id': ObjectId(post_id)}, {'$set': post})
 
     # Update the user's subscriptions in the database
     collection.update_one({'_id': user['_id']}, {
@@ -248,13 +384,19 @@ def get_post():
     post = collection1.find_one({'_id': ObjectId(post_id)})
 
     if post:
+
         # Prepare post information to send back to the client
         post_info = {
             'title': post['title'],
             'thumbnail_link': post['thumbnail'],
             'post_id': post_id,
             'description': post['description'],
-            'permissions': post['permission']
+            'permissions': post['permission'],
+            'username': post['username'],
+            'views': post['views'],
+            'subs': post['subs'],
+            'posts': post['post'],
+            'VIP': post['VIP']
 
 
 
@@ -263,7 +405,38 @@ def get_post():
         print(post['permission'])
         return jsonify(post_info)
     else:
-        return jsonify({'error': 'Post not found'})
+        return jsonify({'error': 'Post not found'}), 400
+# Get messages endpoint\
+
+
+@application.route('/get_vip_post', methods=['POST'])
+def get_vip_post():
+    data = request.get_json()
+    post_id = data.get('post_id')
+
+    # Find the post with the given ID
+    post = collection3.find_one({'_id': ObjectId(post_id)})
+
+    if post:
+
+        # Prepare post information to send back to the client
+        post_info = {
+            'title': post['title'],
+            'thumbnail_link': post['thumbnail'],
+            'post_id': post_id,
+            'description': post['description'],
+            'balance': post['balance'],
+            'sales': post['sales'],
+            'reveneau': post['reveneau']
+
+
+
+
+        }
+
+        return jsonify(post_info)
+    else:
+        return jsonify({'error': 'Post not found'}), 400
 # Get messages endpoint\
 
 
@@ -305,10 +478,13 @@ def get_messages():
 @application.route('/unsubscribe', methods=['POST'])
 @jwt_required()
 def unsubscribe():
+
     current_user = get_jwt_identity()
     data = request.get_json()
+
     # The single post ID the user wants to unsubscribe from
     post_id = data.get('post_id')
+    post = collection1.find_one({'_id': ObjectId(post_id)})
 
     # Find the user with the given username or email
     user = collection.find_one(
@@ -321,6 +497,8 @@ def unsubscribe():
     current_subscriptions = user.get('subscriptions', [])
     if post_id in current_subscriptions:
         current_subscriptions.remove(post_id)
+        post['subs'] -= 1
+        collection1.update_one({'_id': ObjectId(post_id)}, {'$set': post})
     if not current_subscriptions:
         # 204 No Content
         return jsonify({'message': 'No subscriptions found'}), 204
@@ -332,10 +510,78 @@ def unsubscribe():
     return jsonify({'message': 'Unsubscribed successfully', 'subscriptions': current_subscriptions})
 
 
-# ______________________________Change Posts_________________________________
+@application.route('/search_boxes', methods=['POST'])
+def search_boxes():
 
+    data = request.get_json()
+    query = data.get('query')  # The search query (box title)
+    page = data.get('page', 1)  # Default page is 1
+    # Default number of boxes per page is 10
+    per_page = data.get('per_page', 10)
+
+    # Search for boxes matching the query in the database
+    boxes = collection1.find(
+        {'title': {'$regex': query, '$options': 'i'}},
+        {'_id': 1, 'title': 1}
+    ).limit(per_page).skip((page - 1) * per_page)
+
+    # Convert the MongoDB Cursor to a list of dictionaries
+    box_list = list(boxes)
+    print(box_list)
+    for box in box_list:
+        box['_id'] = str(box['_id'])
+    print(box_list)
+
+    if not box_list:
+        # 204 No Content
+        return jsonify({'message': 'No boxes found'}), 204
+
+    return jsonify({'boxes': box_list})
+
+# Rest of your code...
+
+# hello
+
+
+@application.route('/add_views', methods=['POST'])
+def add_view():
+    data = request.get_json()
+    post_id = data.get('post_id')
+
+    # Find the post with the given ID
+    post = collection1.find_one({'_id': ObjectId(post_id)})
+
+    if post:
+        # Increment the view count of the post
+        post['views'] += 1
+        collection1.update_one({'_id': ObjectId(post_id)}, {'$set': post})
+
+        return jsonify({'message': 'View added successfully'}), 200
+    else:
+        return jsonify({'error': 'Post not found'}), 404
 
 # Protected route for modifying posts
+
+
+@application.route('/add_permission', methods=['post'])
+@jwt_required()
+def add_permission():
+
+    data = request.get_json()
+    post_id = data.get('post_id')
+    username = data.get('username')
+    post = collection1.find_one({'_id': ObjectId(post_id)})
+
+    if username not in post['permission']:
+
+        post['permission'].append(username)
+        collection1.update_one({'_id': ObjectId(post_id)}, {'$set': post})
+        return jsonify({'message': 'User added to permissions'})
+    else:
+        collection1.update_one({'_id': ObjectId(post_id)}, {'$set': post})
+        return jsonify({'message': 'User added to permissions'})
+
+
 @application.route('/modify_post', methods=['POST'])
 @jwt_required()
 def modify_post():
@@ -347,9 +593,8 @@ def modify_post():
     action = data.get('action')
     username = data.get('username')
     permissions = data.get('permissions')
-
-    # Find the post with the given ID
     post = collection1.find_one({'_id': ObjectId(post_id)})
+    # Find the post with the given ID
 
     if not post:
         return jsonify({'error': 'Post not found'}), 404
@@ -471,5 +716,43 @@ def get_user():
     else:
         return jsonify({'error': 'User not found'})
 
+
+@application.route('/update_profile', methods=['POST'])
+@jwt_required()
+def update_profile():
+    current_user = get_jwt_identity()
+    data = request.get_json()
+    new_profile_picture = data.get('profile_picture')
+    new_name = data.get('name')
+
+    # Find the user with the given username
+    user = collection.find_one({'username': current_user})
+    posts = collection1.find({'username': current_user})
+    print(posts)
+
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
+    # Update the user's profile picture and name
+    user['profile_picture'] = new_profile_picture
+    user['name'] = new_name
+    try:
+
+        for post in posts:
+            post['profile_image'] = new_profile_picture
+            post['name'] = new_name
+            collection1.update_one({'_id': post['_id']}, {'$set': post})
+
+    except Exception as e:
+        print(e)
+    posts = collection1.find({'username': current_user})
+    print(posts)
+
+    collection.update_one({'_id': user['_id']}, {'$set': user})
+    user = collection.find_one({'username': current_user})
+    print(user)
+    print(new_profile_picture)
+
+    return jsonify({'message': 'Profile updated successfully'})
 
 application.config['SECRET_KEY'] = secret_key
